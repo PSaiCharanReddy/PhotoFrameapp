a52795610d23b8fc1370d0a63de6023b88ea7999
PSaiCharanReddy <psaicharanreddy7920@gmail.com>
2024-04-23 22:04:59 +0530

    added features like changing the pictures

--- app/src/main/java/com/microsoft/photoframeapp/MainActivity.kt
+++ app/src/main/java/com/microsoft/photoframeapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.microsoft.photoframeapp
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.microsoft.photoframeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var currentImage=0
     lateinit var binding:ActivityMainBinding
+    lateinit var image: ImageView
+    val names= arrayOf("Mr Reddy","MR Reddy Logo","Pinkish Wallpaper","Owner Of Mr Reddy Group")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
@@ -15,7 +19,33 @@ class MainActivity : AppCompatActivity() {
         var prev=binding.imgprevious
         val next=binding.imgnext
         prev.setOnClickListener(){
-            val current="pic$currentImage"
+            val currentImageToString="pic$currentImage"
+            val idcurrentImageToString=this.resources.getIdentifier(currentImageToString,"id",packageName)
+        image=findViewById(idcurrentImageToString)
+            image.alpha=0f
+
+            currentImage=(4+currentImage-1)%4;
+            val currentImageToShow="pic$currentImage"
+            binding.name.text=names[currentImage]
+            val idcurrentImageToShowInt=this.resources.getIdentifier(currentImageToShow,"id",packageName)
+            image=findViewById(idcurrentImageToShowInt)
+            image.alpha=1f
+
+        }
+        next.setOnClickListener(){
+            val currentImageToString="pic$currentImage"
+            val idcurrentImageToString=this.resources.getIdentifier(currentImageToString,"id",packageName)
+            image=findViewById(idcurrentImageToString)
+            image.alpha=0f
+
+            currentImage=(4+currentImage+1)%4;
+
+            binding.name.text=names[currentImage]
+            val currentImageToShow="pic$currentImage"
+            val idcurrentImageToShowInt=this.resources.getIdentifier(currentImageToShow,"id",packageName)
+            image=findViewById(idcurrentImageToShowInt)
+            image.alpha=1f
+
         }
     }
 }
